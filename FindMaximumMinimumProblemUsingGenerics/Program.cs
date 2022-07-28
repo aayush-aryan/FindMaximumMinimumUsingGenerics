059aa52c664d415e452957fdc3e4edcf89da6bcd
aayush-aryan <log2aayusharyan@gmail.com>
2022-07-28 16:45:54 +0530

    added welcome message to FindMaximumMiniumUsingGenerics

---
+++ FindMaximumMinimumProblemUsingGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaximumMinimumProblemUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Minimum Problem Using Generics :");
+        }
+    }
+}
